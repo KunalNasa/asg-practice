b94078becb8b601e3c74b515a5979dca6b59666f
KunalNasa <kunalnasa.dev@gmail.com>
2025-04-18 22:18:07 +0530

    completed

--- loadtest.js
+++ loadtest.js
@@ -2,10 +2,10 @@ import { loadTest } from 'loadtest';
 
 // Define the options for the load test
 const options = {
-  url: 'http://asg-practice-asg-lb-580922520.ap-south-1.elb.amazonaws.com/api/10000000000', // your endpoint
-  concurrency: 100,  // Number of concurrent requests to make
-  requestsPerSecond: 100, // Requests per second (this will send 100 requests per second)
-  maxRequests: 1000,  // Maximum number of requests to send in total (optional)
+  url: 'http://asg-practice-asg-1-1289438382.ap-south-1.elb.amazonaws.com/api/1000000000', // your endpoint
+  concurrency: 10,  // Number of concurrent requests to make
+  requestsPerSecond: 10, // Requests per second (this will send 100 requests per second)
+  maxRequests: 100,  // Maximum number of requests to send in total (optional)
   method: 'GET', // HTTP method (GET or POST, depending on your endpoint)
   // Optional headers (if needed, like for authorization)
   // headers: {
@@ -23,3 +23,4 @@ loadTest(options, function(error, result) {
 
   console.log('Load test result:', result);
 });
+
